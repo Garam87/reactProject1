2a2f15861ceefcc80c91defa88ba8d39b694e439
Garam87 <115548822+Garam87@users.noreply.github.com>
2023-07-17 21:32:03 +0900

    2. 전체 상품 페이지에서는 전체 상품을 볼수 있다.

---
+++ src/component/ProductCard.js
@@ -0,0 +1,12 @@
+const ProductCard = ({ item }) => {
+  return (
+    <div>
+      <img src={item?.img} />
+      <div>{item?.title}</div>
+      <div>₩{item?.price}</div>
+      <div>{item?.new == true ? "신제품" : ""}</div>
+    </div>
+  );
+};
+
+export default ProductCard;
